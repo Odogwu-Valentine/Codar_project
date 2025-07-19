ea897e9fa2f0ad19ccc3ab90f35e0e060d8a9b45
valentine <valentineodogwu@gmail.com>
2025-07-19 15:47:45 +0100

    updated index html and the vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Codar_project/',
-  server: {
-    port: 3000,
-    open: true,
-  },
+
   plugins: [react()],
 })
